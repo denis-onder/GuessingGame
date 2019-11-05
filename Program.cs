ee846b59761a1006f417206f7c849fa93b9e4983
Denis Onder <denis_onder@protonmail.com>
2019-11-05 16:01:55 +0100

    Logic

--- Program.cs
+++ Program.cs
@@ -3,20 +3,36 @@
 namespace GuessingGame
 {
   class Logic {
-    static int UserInput;
-     static void Start() {
+    private int UserInput;
+    private int Result = 10;
+     public void Start() {
+      /**
+       * Ask for user input
+       * If the input is not valid, call this method again
+       * Otherwise, go ahead and proceed on with the game.
+       */
         Console.WriteLine("Greetings! Please provide a number in the range of 1 - 100:");
-        const dynamic input = Console.ReadLine();
+        int input = int.Parse(Console.ReadLine());
         if(input < 1 || input > 100) {
         Console.WriteLine("Invalid input.");
+        Start();
+      } else {
+        UserInput = input;
+        Guess();
       }
+    }
+    private bool Guess() {
+      if(UserInput == Result) {
+        return true;
+      }
+      return false;
     }
   }
   class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      new Logic().Start();
     }
   }
 }
