87e5c518e96ac93580bd6d4e4847305c3b1b77c4
Denis Onder <denis_onder@protonmail.com>
2019-11-05 16:25:34 +0100

    Game is working

--- Program.cs
+++ Program.cs
@@ -2,27 +2,44 @@
 
 namespace GuessingGame
 {
-  class Logic {
+  class Logic
+  {
     private int UserInput;
     private int Result = 10;
-     public void Start() {
+    public void Start()
+    {
       /**
        * Ask for user input
        * If the input is not valid, call this method again
        * Otherwise, go ahead and proceed on with the game.
        */
-        Console.WriteLine("Greetings! Please provide a number in the range of 1 - 100:");
-        int input = int.Parse(Console.ReadLine());
-        if(input < 1 || input > 100) {
+      Console.WriteLine("Greetings! Please provide a number in the range of 1 - 100:");
+      int input = int.Parse(Console.ReadLine());
+      if (input < 1 || input > 100)
+      {
         Console.WriteLine("Invalid input.");
-        Start();
-      } else {
+        Console.ReadLine();
+      }
+      else
+      {
         UserInput = input;
-        Guess();
+        bool result = Guess();
+        if (result)
+        {
+          Console.WriteLine("Good job!");
+          Console.ReadLine();
+        }
+        else
+        {
+          Console.WriteLine("Wrong guess!");
+          Console.ReadLine();
+        }
       }
     }
-    private bool Guess() {
-      if(UserInput == Result) {
+    private bool Guess()
+    {
+      if (UserInput == Result)
+      {
         return true;
       }
       return false;
