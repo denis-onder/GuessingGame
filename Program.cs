446bae36149e92140998cc99fbea7c0035e93554
Denis Onder <denis_onder@protonmail.com>
2019-11-05 15:40:01 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GuessingGame
+{
+  class Logic {
+    static int UserInput;
+     static void Start() {
+        Console.WriteLine("Greetings! Please provide a number in the range of 1 - 100:");
+        const dynamic input = Console.ReadLine();
+        if(input < 1 || input > 100) {
+        Console.WriteLine("Invalid input.");
+      }
+    }
+  }
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
